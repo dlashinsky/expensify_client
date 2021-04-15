802e68528cd044ea5c703a967e9324ea68ba7e7b
dlashinsky <da.lashinsky@gmail.com>
2021-04-14 17:42:18 -0700

    finishing boiler plate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+import Home from './components/Home'
+import Login from './components/Login'
 import './App.css';
 
 function App() {
-  console.log(process.env.REACT_APP_SERVER_URL)
+  // user data will be in state when the user is logged in 
+  const[currentUser, setCurrentUser] = useState(null)
+
+  //useEffect if the user navigates away from the site and comes back
+
+  //deletes the jwt from local storage when teh user wants to log out
+  const handleLogout = () => {
+    console.log('log the user out')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar currentUser={ currentUser } handleLogout={ handleLogout } />
       </header>
-    </div>
-  );
+
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={ Home }/>
+
+          <Route 
+            path="/register"
+            render={ (props) => <Register {...props} currentUser={ currentUser } setCurrentUser={ setCurrentUser} /> }
+          />
+
+          <Route 
+            path="/login"
+            render={ (props) => <Login {...props} currentUser={ currentUser } setCurrentUser={ setCurrentUser} /> }
+          />
+        
+
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
